e214a09eee3001c4f0d1e3f0f0b38b8eeedaa36b
Vlad Kudin <vladk@Vlads-MacBook-Air-675.local>
2025-09-25 18:08:45 -0400

    delete_need

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
@@ -1,19 +1,23 @@
 package com.ufund.api.ufundapi.controller;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.ufund.api.ufundapi.model.Need;
+
 public class NeedController {
     // @DeleteMapping("/{id}")
-    // public ResponseEntity<Hero> deleteHero(@PathVariable int id) {
+    // public ResponseEntity<Need> deleteHero(@PathVariable int id) {
     //     LOG.info("DELETE /heroes/" + id);
 
     //     try{
-    //         Hero[] heroes = heroDao.getHeroes();
+    //         Need[] heroes = heroDao.getHeroes();
     //         for(Hero hero : heroes){
     //             if(hero.getId() == id){
     //                 return new ResponseEntity<>(hero, HttpStatus.OK);
