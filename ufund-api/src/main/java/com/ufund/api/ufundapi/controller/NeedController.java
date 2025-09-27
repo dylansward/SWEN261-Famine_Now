5276244a090651ab596a971e36ee9714dcd3ccd9
DylanSward <dylansward06@gmail.com>
2025-09-27 15:25:27 -0400

    Starting DAO Implementation

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
@@ -1,5 +1,19 @@
 package com.ufund.api.ufundapi.controller;
 
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufund.api.ufundapi.persistence.NeedDAO;
+
+@RestController
+@RequestMapping("needs")
 public class NeedController {
-    
+    private static final Logger LOG = Logger.getLogger(NeedController.class.getName());
+    private NeedDAO needDAO;
+
+    public NeedController(NeedDAO needDAO) {
+        this.needDAO = needDAO;
+    }
 }
--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedFileDAO.java
@@ -1,5 +1,75 @@
 package com.ufund.api.ufundapi.persistence;
 
-public class NeedFileDAO {
-    
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.ufund.api.ufundapi.model.Need;
+
+@Component
+public class NeedFileDAO implements NeedDAO {
+    private static final Logger LOG = Logger.getLogger(NeedFileDAO.class.getName());
+    Map<Integer, Need> needs;
+    private ObjectMapper objectMapper;
+    private static int nextId;
+    private String filename;
+
+    public NeedFileDAO(@Value("${need.file}") String filename, ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+        load();
+    }
+
+    private synchronized static int nextId() {
+        int id = nextId;
+        ++nextId;
+        return id;
+    }
+
+    private Need[] getNeedsArray() {
+        return getNeedsArray(null);
+    }
+
+    private Need[] getNeedsArray(String containsText) {
+        ArrayList<Need> needArrayList = new ArrayList<>();
+        for (Need need : needs.values()) {
+            if (containsText == null || need.getName().contains(containsText)) {
+                needArrayList.add(need);
+            }
+        }
+
+        Need[] needArray = new Need[needArrayList.size()];
+        needArrayList.toArray(needArray);
+        return needArray;
+    }
+
+    private boolean save() throws IOException {
+        Need[] needArray = getNeedsArray();
+        objectMapper.writeValue(new File(filename), needArray);
+        return true;
+    }
+
+    private boolean load() throws IOException {
+        needs = new TreeMap<>();
+        nextId = 0;
+
+        Need[] needArray = objectMapper.readValue(new File(filename), Need[].class);
+        for (Need need : needArray) {
+            needs.put(need.getId(), need);
+            if (need.getId() >= nextId) {
+                nextId = need.getId();
+            }
+        }
+
+        ++nextId;
+        return true;
+    }
 }
