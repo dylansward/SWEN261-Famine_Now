30dc80134455490c52af474f7c7cc07f181671c3
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2025-10-09 18:56:31 -0400

    added routed buttons to app

--- ufund-ui/ufund-angular/src/app/app-routing-module.ts
+++ ufund-ui/ufund-angular/src/app/app-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './login-page/login-page';
+import { Needs } from './needs/needs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login-page', component: LoginPage},
+  {path: 'cupboard', component: Needs}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
