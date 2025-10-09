60172802b1a9029eba319b8a5a2bfb4f70a5177c
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2025-10-09 17:32:16 -0400

    i think i got angular to work right

--- ufund-ui/ufund-angular/src/app/app-module.ts
+++ ufund-ui/ufund-angular/src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Needs } from './needs/needs';
+import { LoginPage } from './login-page/login-page';
 
 @NgModule({
   declarations: [
     App,
-    Needs
+    Needs,
+    LoginPage
   ],
   imports: [
     BrowserModule,
