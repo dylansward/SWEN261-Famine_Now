9389b384c8109a14f213bac4e573403d521a86a1
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2025-10-21 20:49:09 -0400

    text fields work

--- ufund-ui/ufund-angular/src/app/app-module.ts
+++ ufund-ui/ufund-angular/src/app/app-module.ts
@@ -7,6 +7,7 @@ import { App } from './app';
 import { Needs } from './needs/needs';
 import { LoginPage } from './login-page/login-page';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
     NgFor,
+    FormsModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- ufund-ui/ufund-angular/src/app/needs/needs.ts
+++ ufund-ui/ufund-angular/src/app/needs/needs.ts
@@ -1,15 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import { Need } from '../need';
 import { BackendConnection } from '../backend-connection';
+import {FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-needs',
   standalone: false,
   templateUrl: './needs.html',
-  styleUrl: './needs.css',
+  styleUrl: './needs.css'
 })
 export class Needs implements OnInit {
-  needs: Need[] = []
+  needs: Need[] = [];
+  curr_id: number = 0;
+  curr_name: string = "";
+  curr_cost: number = 0.0;
+  curr_quanity: number = 0;
 
   constructor(private backend: BackendConnection) { }
 
@@ -25,5 +31,20 @@ export class Needs implements OnInit {
     console.log("start editing");
     console.log(n.name);
     n.current = true;
+    this.curr_id = n.id;
+    this.curr_name = n.name;
+    this.curr_cost = n.cost;
+    this.curr_quanity = n.quantity;
+  }
+
+  updateEdit(n: Need): void {
+    console.log("end editing");
+    console.log(n.name);
+    n.current = false;
+  }
+
+  discardEdit(n: Need): void {
+    console.log("end editing");
+    n.current = false;
   }
 }
