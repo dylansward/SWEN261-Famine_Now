9b6e2eb712cf07066b7b315fa02306364dbaf992
Vlad Kudin <vladk@Vlads-MacBook-Air-675.local>
2025-11-06 17:02:46 -0500

    10%cases

--- ufund-ui/ufund-angular/src/app/lootbox/lootbox.ts
+++ ufund-ui/ufund-angular/src/app/lootbox/lootbox.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-lootbox',
   standalone: false,
   templateUrl: './lootbox.html',
-  styleUrl: './lootbox.css'
+  styleUrls: ['./lootbox.css']
 })
-export class Lootbox {
-  
+export class Lootbox implements AfterViewInit {
+  @ViewChild('wheel') wheel!: ElementRef;
+
+  results = [
+    "Blue header",
+    "Halloween body",
+    "Pink buttons",
+    "Red textarea",
+    "Purple text",
+    "Large font",
+    "Small font",
+    "New buttons"
+  ];
+
+  ngAfterViewInit() {}
+
+    spinButton() {
+    const value = Math.ceil(Math.random() * 3600);
+    this.wheel.nativeElement.style.transform = `rotate(${value}deg)`;
+
+    const normalized = value % 360;
+
+    const corrected = (360 - normalized + 22.5) % 360;
+    const index = Math.floor(corrected / 45);
+
+    const result = this.results[index];
+    console.log("Result:", result);
+  }
+
+
 }
